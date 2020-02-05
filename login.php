133297b692cefa659c1ddf709214c4e76ac0d622
Kenneth Solomon <synecders@gmail.com>
2020-02-06 00:00:44 +0800

    redesign login form

--- login.php
+++ login.php
@@ -56,7 +56,7 @@ include_once('./includes/connection.php');
                     }
 
                     ?>
-                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                    <h1 class="h4 text-gray-900 mb-5">Welcome Back!</h1>
                   </div>
                   <form action="./includes/login.php" method="POST" class="user">
                     <div class="form-group">
@@ -68,25 +68,12 @@ include_once('./includes/connection.php');
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
                         <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                        <!-- <label class="custom-control-label" for="customCheck">Remember Me</label> -->
                       </div>
                     </div>
                     <button type="submit" name="btnLogin" class="btn btn-primary btn-user btn-block">Login</button>
                     <hr>
-                    <a href="index.html" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a>
                   </form>
-                  <hr>
-                  <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
-                  </div>
                 </div>
               </div>
             </div>
