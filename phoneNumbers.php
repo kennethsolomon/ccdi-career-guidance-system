ad218d6073f4ccd5f435bd8fd5f1476a1ba075cd
Kenneth Solomon <synecders@gmail.com>
2020-02-09 17:06:02 +0800

    fix broken links

--- phoneNumbers.php
+++ phoneNumbers.php
@@ -17,7 +17,7 @@
      <meta name="description" content="">
      <meta name="author" content="">
 
-     <title>SB Admin 2 - Forgot Password</title>
+     <title>CGS - CCDI</title>
 
      <!-- Custom fonts for this template-->
      <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
